6a0ce9738c1d30537a1eed721cd144c5af02a0ba
Levi Borland <borland.levi@gmail.com>
2019-08-16 02:06:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,21 @@ def oxford_comma(array)
     when 2 
       return array.join(" and ")
     when 3 
-
+      
+      #Remove final Element from Array
+      final = array.pop 
+      string = ""
+      
+      #Iterate through Array to add Commas
+      array.each do |element|
+        string << element + ", " 
+      end
+      
+      
+      #Add the last element onto the String
+      string << "and " + final 
+      
+      return string
+      
   end
 end
