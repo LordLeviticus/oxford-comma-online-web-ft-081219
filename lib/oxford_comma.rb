7a01196a4f24ed5dc93b440d3379df23d19d4cc6
Levi Borland <borland.levi@gmail.com>
2019-08-16 01:31:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   case array.length
-  when 1 
-    array.split(", and ")
-  when 2 
-    array.split("and ")
-  when 3
+    when 1 
+     array.split(", and ")
+    when 2 
+      array.split("and ")
+  end
 end
